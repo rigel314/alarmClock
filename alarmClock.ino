9c73c29d2a51f32864a0ae93498c7bb905a9666d
Cody Creager <pi.rubiks@gmail.com>
2017-12-24 21:53:07 -0800

    Add bird noise PoC

--- alarmClock.ino
+++ alarmClock.ino
@@ -3,6 +3,7 @@
 #include <TimeLib.h>
 #include <DS1307RTC.h>
 #include <Wire.h>
+#include <avr/pgmspace.h>
 
 #define BUT_PIN1 A0
 #define BUT_PIN2 A1
@@ -17,6 +18,8 @@
 #define SCRN_RST_PIN 10
 #define SCRN_SCE_PIN 12
 #define SCRN_LED_PIN 6 // PWM
+
+#define MCP4726_ADDR 0x62
 
 #define DEBUG // for now
 
@@ -58,6 +61,9 @@
 volatile int gotTimer1 = 0;
 LCD lcd(SCRN_SCLK_PIN, SCRN_MOSI_PIN, SCRN_DC_PIN, SCRN_RST_PIN, SCRN_SCE_PIN);
 
+const char sound[] PROGMEM =
+#include "bird.h" // This has the data and a semicolon
+
 // Called via timer interrupt
 void timer1inter()
 {
@@ -83,7 +89,11 @@ void setup()
 	lcd.sendString(0, 0, "Testing...");
 	
 	setSyncProvider(RTC.get);
+	setTime(RTC.get());
 	// setSyncInterval(10);
+	
+	// TWBR = ((F_CPU / 400000L) - 16) / 2; // Set I2C frequency to 400kHz
+	Wire.setClock(400000L);
 	
 	pinMode(BUT_PIN1, INPUT);
 	pinMode(BUT_PIN2, INPUT);
@@ -98,30 +108,35 @@ void loop()
 	static int prevBut = 0;
 	static int prev1Val = 0;
 	static int prev2Val = 0;
+	static int backlight = 0;
 	int but;
 	
-	static int setctr = 0;
+	volatile char blar = sound[0];
+	if(blar)
+	{
+		log("WTF");
+	}
 	
 	if(gotTimer1)
 	{
 		gotTimer1 = 0;
 		tmElements_t tm;
-
+		
 		breakTime(now(), tm);
 		
-		Serial.print("Ok, Time = ");
-		print2digits(tm.Hour);
-		Serial.write(':');
-		print2digits(tm.Minute);
-		Serial.write(':');
-		print2digits(tm.Second);
-		Serial.print(", Date (D/M/Y) = ");
-		Serial.print(tm.Day);
-		Serial.write('/');
-		Serial.print(tm.Month);
-		Serial.write('/');
-		Serial.print(tmYearToCalendar(tm.Year));
-		Serial.println();
+		// Serial.print("Ok, Time = ");
+		// print2digits(tm.Hour);
+		// Serial.write(':');
+		// print2digits(tm.Minute);
+		// Serial.write(':');
+		// print2digits(tm.Second);
+		// Serial.print(", Date (D/M/Y) = ");
+		// Serial.print(tm.Day);
+		// Serial.write('/');
+		// Serial.print(tm.Month);
+		// Serial.write('/');
+		// Serial.print(tmYearToCalendar(tm.Year));
+		// Serial.println();
 	}
 	
 	int but1Val = analogRead(BUT_PIN1);
@@ -171,6 +186,15 @@ void loop()
 		analogWrite(RED_PIN, 64);
 		analogWrite(GRN_PIN, 64);
 		analogWrite(BLU_PIN, 64);
+		for(int i = 0; i < 4096; i++)
+		{
+			Wire.beginTransmission(MCP4726_ADDR);
+			// logobj(val);
+			Wire.write(i >> 8 & 0xFF);
+			Wire.write(i & 0xFF);
+			Wire.endTransmission(true);
+			delay(1);
+		}
 	}
 	else if(but == 4 && but != prevBut)
 	{
@@ -178,11 +202,27 @@ void loop()
 		analogWrite(RED_PIN, 128);
 		analogWrite(GRN_PIN, 128);
 		analogWrite(BLU_PIN, 128);
+		
+		const char* ptr = sound;
+		for(int i = 0; i < 16000; i++)
+		{
+			Wire.beginTransmission(MCP4726_ADDR);
+			int16_t val = pgm_read_byte(ptr++);
+			val += 128;
+			val <<= 3;
+			// logobj(val);
+			Wire.write(val >> 8 & 0x07);
+			Wire.write(val & 0xFF);
+			Wire.endTransmission(true);
+			delayMicroseconds(15);
+		}
+		// Wire.endTransmission(true);
 	}
 	else if(but == 5 && but != prevBut)
 	{
 		logwobj("B5", but2Val);
-		analogWrite(SCRN_LED_PIN, 64);
+		backlight = !backlight;
+		analogWrite(SCRN_LED_PIN, (backlight) ? 2 : 0);
 	}
 	else if(but == 0 && but != prevBut)
 	{
